cd652b8c9166f76d6abf4ec0a60a06a94a046f23
andesontiban <92953060+andersontiban@users.noreply.github.com>
2022-01-06 16:24:32 -0500

    Add files via upload

---
+++ webscraper.py
@@ -0,0 +1,18 @@
+import pandas as pd
+from bs4 import BeautifulSoup
+from selenium import webdriver
+driver = webdriver.Chrome(executable_path= "C:/Users/andyt/Downloads/chromedriver_win32")
+driver.get("https://oxylabs.io/blog")
+
+results = []
+other_results = []
+content = driver.page_source
+soup = BeautifulSoup(content)
+
+driver.quit()
+for element in soup.findAll(attrs="blog-card__content-wrapper"):
+    name = element.find('h2')
+    if name not in results:
+        results.append(name.text)
+df = pd.DataFrame({'Names': results})
+df.to_csv('names.csv', index=False, encoding = 'utf-8')
